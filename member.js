63085b2742ef0385ca1ac92d14961f709acbab54
Stijn Peeters <schwarzie2478@hotmail.com>
2024-05-10 08:14:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    var skills = ["HTML", "CSS", "JS"];
+    console.log("Member skills: ");
+    for(var i=0; i<skills.length; i++){
+        console.log(skills[i]);
+    }
+}
