9f0870a85b3a7dd51c520326eb8aa8df65b1adeb
Stijn Peeters <schwarzie2478@hotmail.com>
2024-05-10 08:19:20 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,33 @@
+// create webserver
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function (request, response) {
+    var urlObj = url.parse(request.url, true);
+    var pathname = urlObj.pathname;
+    if (pathname === '/') {
+        fs.readFile('./index.html', function (err, data) {
+            if (err) {
+                console.log(err);
+                response.end('read file index.html error');
+            } else {
+                response.end(data);
+            }
+        });
+    } else if (pathname === '/submit') {
+        var comment = urlObj.query;
+        comments.push(comment);
+        response.end(JSON.stringify(comments));
+    } else {
+        fs.readFile('.' + pathname, function (err, data) {
+            if (err) {
+                console.log(err);
+                response.end('read file error');
+            } else {
+                response.end(data);
+            }
+        });
+    }
+});
